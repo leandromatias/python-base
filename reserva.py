a789902aa88ad8c61e20d0521d61b25eb791cc92
Leandro Matias <leandrozmatias@gmail.com>
2022-12-15 18:07:13 +0000

    concluindo revisao de exercicio

--- reserva.py
+++ reserva.py
@@ -5,6 +5,9 @@ import logging
 import sys
 import pprint
 
+
+# Acesso ao banco de dados / parsing
+# TODO: Usar função para ler os arquivos
 ocupados = {}
 try:
     for line in open("reservas.txt"):
@@ -30,24 +33,32 @@ except FileNotFoundError:
     logging.error("Arquivo `quartos.txt` não existe.")
     sys.exit(1) 
 
+# Programa principal
 quartos_items = quartos.items()
-
 print("Reserva Hotel Pythonico")
-print("-" * 40)
+print("-" * 60)
 if len(ocupados) == len(quartos):
+    # TODO: sistema de aviso
     print("Hotel lotado, tente outro dia.")
-    sys.exit(1)
+    sys.exit(0)
 # TODO: Tratar exceptions do nome_cliente
 nome_cliente = input("Nome do cliente: ").strip()
-print("-" * 40)
+print("-" * 60)
 print("Lista de quartos:")
-
+# TODO: usar lib Rich
+print()
+head = ["Número", "Nome do Quarto", "Preço", "Disponível"]
+print(f"{head[0]:<6} - {head[1]:<14} - R$ {head[2]:<9} - {head[3]:<10}")
+print("-" * 60)
 for codigo, dados in quartos_items: # key(codigo) values(dados)
     nome_quarto = dados["nome"]
     preco_diaria = dados["preco"]
     disponivel = "Indisponível" if not dados["disponivel"] else "Disponível"
-    print(f"{codigo} - {nome_quarto} - R$ {preco_diaria:.2f} - {disponivel}.")
-print("-" * 40)
+    print(
+        f"{codigo:<6} - {nome_quarto:<14} - "
+        f"R$ {preco_diaria:<9.2f} - {disponivel:<10}."
+    )
+print("-" * 60)
 # TODO: for para iterar as duas perguntas
 try:
     num_quarto = int(input("Número do quarto: ").strip())
@@ -69,12 +80,11 @@ except ValueError:
     sys.exit(1)
 
 nome_quarto = quartos[num_quarto]["nome"]
-disponivel = quartos[num_quarto]["disponivel"]
 preco_quarto = quartos[num_quarto]["preco"]
 total = preco_quarto * dias
 
 with open("reservas.txt", "a") as file_:
     file_.write(f"{nome_cliente},{num_quarto},{dias}\n")
-
+# TODO: sistema de confirmação
 print("Reservado!")
 print(f"{nome_cliente}, você escolheu o {nome_quarto} e vai custar {total:.2f}.")
